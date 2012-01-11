cabd2fad0dc322e6cd88877b164c68b38a972b8b
andres <andres.f.perez@gmail.com>
2012-01-10 20:59:26 -0500

    Removed the forms scripting and styling

--- src/shaving/js/main.js
+++ src/shaving/js/main.js
@@ -725,44 +725,6 @@ function renderShavingJournalEntry(entry_elem, entry, num_displayed) {
 	return true;
 }
 
-/* FORMS */
-function loadForms() {
-	// Journal entry form
-	init_form("journal_form_date");
-	loadUsedProducts();
-	createJournalGradeScales();
-
-	// Set the width for the tabbed spans
-	var spans = document.getElementById('forms').getElementsByTagName('div');
-	// Grab all spans
-	for (var i = spans.length-1; i >= 0; i--) {
-		var span_classes = spans[i].getAttribute('class');
-		// Search for prod_type_lists class in each span
-		if (span_classes != null && span_classes.search("prod_type_lists") != -1) {
-			// Get its parent cell
-			// Retrieve all selects
-			var max_list_width = 0;
-			var span_lists = spans[i].getElementsByTagName('select');
-			for (var j = span_lists.length-1; j >= 0; j--) {
-				//console.log("WIDTH1 for '", span_lists[j].id,"': ", span_lists[j].clientWidth);
-				var width = span_lists[j].clientWidth;
-				// Find max width of all selects
-				// Set parent cell's width to max of selects
-				if (width > max_list_width) {
-					setStyle(spans[i], "width: "+width+"px !important;");
-					max_list_width = width;
-				}
-			}
-		}
-	}
-	
-	// Review form
-	init_form("review_form_date");
-	loadReviewGrades("Razor", "review_grade_scales");
-	var prodIDField = document.getElementById("review_entry_id");
-	if (prodIDField != null) prodIDField.value = HG_getNextProductID(ShavingReviewedProducts);
-}
-
 /* FILTERING */
 function filterData(filter) {
 	var tab = tabsObj.CurrentTab;
@@ -930,19 +892,6 @@ function onTabToggle(from, to) {
 		break;
 	case 2: // Trends
 			break;
-	case 3: // Submit Entry
-		// unselect all tabbed product lists
-		for (var i = tabbed_prod_lists.length-1; i >= 0; i--) {
-			var list = document.getElementById(tabbed_prod_lists[i]);
-			var list_classes = list.getAttribute("class");
-			if (list_classes != null && list_classes.search("sel_prod_type_list") != -1) {
-				list.setAttribute("class", 
-								  list_classes.replace(/sel_prod_type_list/g, ""));
-			}
-		}
-
-		break;
-	
 	default: break;
 	}
 
@@ -971,41 +920,6 @@ function onTabToggle(from, to) {
             if (tabsObj.NestedTabs.length >= 4)
 			    tabsObj.NestedTabs[3].IgnoreKeyboard = false; 
 			break;
-	case 3: // Submit Entry
-			// TODO: select tabbed lists based on selected labels
-			// sel_prod_type_label, sel_prod_type_list
-			for (var i = tabbed_prod_lists.length-1; i >= 0; i--) {
-				var label = document.getElementById(tabbed_prod_lists[i]+"_label");
-				var label_classes = label.getAttribute("class");
-				if (label_classes != null && 
-					label_classes.search("sel_prod_type_label") != -1) {
-					var list = document.getElementById(tabbed_prod_lists[i]);
-					var list_classes = list.getAttribute("class");
-					if (list_classes != null && list_classes.length > 0) 
-						list_classes += " ";
-					else if (list_classes == null)
-						list_classes = "";
-					list.setAttribute("class", list_classes+"sel_prod_type_list");
-				}
-			}
-
-	        var lastEntry = ShavingJournalEntries[ShavingJournalEntries.length-1];
-            console.log("  >> LAST ENTRY: ", lastEntry);
-	        // Blade vs. Strop
-	        if (lastEntry.Blade == null) switchToStrop();
-	        else switchToBlade();
-
-    	    // Cream vs. Soap
-    	    if (lastEntry.Soap == null) switchToCream();
-	        else switchToSoap();
-
-			// create the picture selection control
-			if (pw_sel_obj == null) {
-				pw_sel_obj = PicasaWeb_PicSelect(ShavingAlbum, 
-												document.getElementById("pictures_select"),
-												 null, 
-												 true);
-			}
 	case 2: // Trends
 	default:
 			setStyle(filterLyr, "visibility: hidden;");
@@ -1026,14 +940,7 @@ function loadExtras() {
     if (ShavingCombos == null || ShavingCombos.length <= 0) createShavingCombos();
 	//loadTrends();
 
-	var exclusion_list = null;
-	if (window.location.hostname == "localhost") {
-		loadForms();
-	} else {
-		exclusion_list = ["Trends", "Submit Entry", "TODO"];
-		var forms = document.getElementById("forms");
-		forms.parentNode.parentNode.removeChild(forms.parentNode);
-	}
+	var exclusion_list = exclusion_list = ["Trends"];
 
                                              // function() {
 	                                         //  loadShavingJournal(0, "DESC", null);
